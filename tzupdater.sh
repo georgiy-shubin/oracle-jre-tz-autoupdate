4059683baf784a2b2c58c3b388ee171b7f8cf253
Georgiy Shubin <Georgiy Shubin>
2016-08-02 20:20:46 +0400

    File not found error fixed

--- tzupdater.sh
+++ tzupdater.sh
@@ -2,19 +2,20 @@
 TZUPD_HOME=`pwd`
 SEARCHPATH=$1
 TZDATALINK=$2
-LOCAL_FILE=file://`ls -1t $TZUPD_HOME/tzdata/tzdata*.tar.gz | head -1`
 case "$TZDATALINK" in
-        '')
-                TZDATA=$LOCAL_FILE
-        ;;
         'http'*|'HTTP'*)
                 FILENAME=`echo $TZDATALINK | rev | cut -d"/" -f 1 | rev`
                 wget -P $TZUPD_HOME/tzdata/ $TZDATALINK
                 for i in $FILENAME; do sha512sum $TZUPD_HOME/tzdata/$i | awk '{print $1}' > $TZUPD_HOME/tzdata/$i.sha512; done
                 TZDATA=file://$TZUPD_HOME/tzdata/$FILENAME
         ;;
-        *)
+        'tzdata'*'.tar.gz')
                 TZDATA=file://$TZUPD_HOME/tzdata/$TZDATALINK
+        ;;
+        *)
+                echo '  Please specify a correct filename or URL'
+                echo
+                exit
         ;;
 esac
 case "$3" in
